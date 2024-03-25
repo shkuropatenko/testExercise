8e0668ed9d04b3fab23e3ca4b6866b43cf7f50a9
DmytroShk <dmytro.shkuropatenko@jelvix.com>
2024-03-24 22:50:17 -0400

    Added the second task

--- 1/index.php
+++ 1/index.php
@@ -6,60 +6,74 @@
 </head>
 <body>
 
-<?php
+<!-- Задачи на общий уровень владения языком
+Условия:
+Задачи должны быть решены с помощью кода(PHP, Python, etc).
+Важное условие. избегайте использования циклов for / foreach в решениях.
+Все задачи основаны на работе с многомерным массивом, где элементы могут иметь повторяющиеся значения id:
+$array = [
+  	[id => 1, create => "14.04.2023", title => "array1" ...],
+	  [id => 4, create => "09.02.2023", title => "array4" ...],
+  	[id => 2, create => "03.07.2023", title => "array2" ...],
+    [id => 1, create => "22.04.2023", title => "array1" ...],
+  	[id => 2, create => "12.12.2023", title => "array4" ...],
+  	[id => 3, create => "04.04.2023", title => "array3" ...]
+]
+Создайте PHP скрипт, который выполняет каждую из вышеуказанных задач и выводит результаты. Убедитесь, что ваш код читаем, организован, и соответствует общепринятым стандартам кодирования. -->
 
-  $array = [
-    ['id' => 1, 'create' => "14.04.2023", 'title' => "array1"],
-    ['id' => 4, 'create' => "09.02.2023", 'title' => "array4"],
-    ['id' => 2, 'create' => "03.07.2023", 'title' => "array2"],
-    ['id' => 1, 'create' => "22.04.2023", 'title' => "array1"],
-    ['id' => 2, 'create' => "12.12.2023", 'title' => "array4"],
-    ['id' => 3, 'create' => "04.04.2023", 'title' => "array3"]
-  ];
 
-  // Filtering unique records
-  $uniqueArray = array_values(array_map("unserialize", array_unique(array_map("serialize", $array))));
-  print_r($uniqueArray);
+  <?php
+    $array = [
+      ['id' => 1, 'create' => "14.04.2023", 'title' => "array1"],
+      ['id' => 4, 'create' => "09.02.2023", 'title' => "array4"],
+      ['id' => 2, 'create' => "03.07.2023", 'title' => "array2"],
+      ['id' => 1, 'create' => "22.04.2023", 'title' => "array1"],
+      ['id' => 2, 'create' => "12.12.2023", 'title' => "array4"],
+      ['id' => 3, 'create' => "04.04.2023", 'title' => "array3"]
+    ];
 
-  // Sorting a multidimensional array
-  usort($array, function($a, $b) {
-      return $a['id'] <=> $b['id'];
-  });
+    // Filtering unique records
+    $uniqueArray = array_values(array_map("unserialize", array_unique(array_map("serialize", $array))));
+    print_r($uniqueArray);
 
-  // Filtering by conditions
-  $idToFilter = 2;
-  $filteredArray = array_filter($array, function($element) use ($idToFilter) {
-      return $element['id'] == $idToFilter;
-  });
+    // Sorting a multidimensional array
+    usort($array, function($a, $b) {
+        return $a['id'] <=> $b['id'];
+    });
 
-  // Changing the Array Structure
-  $transformedArray = array_column($array, 'id', 'title');
+    // Filtering by conditions
+    $idToFilter = 2;
+    $filteredArray = array_filter($array, function($element) use ($idToFilter) {
+        return $element['id'] == $idToFilter;
+    });
 
-?>
+    // Changing the Array Structure
+    $transformedArray = array_column($array, 'id', 'title');
+  ?>
 
-<?php
-  // Output of results
-  echo "Sorting a multidimensional array by key 'id':\n";
-  print_r($array);
-  echo "\n";
-?>
-<br>
-<hr>
-<?php
-  // Output of results
-  echo "Filtering elements by condition (id = $idToFilter):\n";
-  print_r($filteredArray);
-  echo "\n";
+  <?php
+    // Output of results
+    echo "Sorting a multidimensional array by key 'id':\n";
+    print_r($array);
+    echo "\n";
+  ?>
+  <br>
+  <hr>
+  <?php
+    // Output of results
+    echo "Filtering elements by condition (id = $idToFilter):\n";
+    print_r($filteredArray);
+    echo "\n";
 
-?>
-<br>
-<hr>
-<?php
-  // Output of results
-  echo "Changing the Array Structure:\n";
-  print_r($transformedArray);
-  echo "\n";
-?>
-
+  ?>
+  <br>
+  <hr>
+  <?php
+    // Output of results
+    echo "Changing the Array Structure:\n";
+    print_r($transformedArray);
+    echo "\n";
+  ?>
 </body>
 </html>
+
